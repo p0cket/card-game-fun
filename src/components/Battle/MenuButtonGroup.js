089ae87567b06203fde48410763c4f4d16cb19f2
pocket <p0ck31@gmail.com>
2023-09-27 23:28:07 -0400

    fix: menu button styles, and template for theme.js

---
+++ src/components/Battle/MenuButtonGroup.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { clockEmoji, energyEmoji } from "../../consts/consts";
+
+function MenuButtonGroup({ togglePopup }) {
+  return (
+    <div
+      style={{
+        fontFamily: "Silkscreen",
+        display: "flex",
+        color: "white",
+        backgroundColor: "#5a7d2a",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          fontSize: "20px",
+          alignContent: "center",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          padding: "4px",
+        }}
+      >
+        {/* 30{clockEmoji}  */}
+        10{energyEmoji.repeat(10)}
+      </div>
+      <div
+        style={{
+          fontFamily: "Silkscreen",
+          display: "flex",
+          justifyContent: "space-around",
+          flex: 2,
+          backgroundColor: "#5a7d2a",
+          color: "white",
+          margin: "3px",
+        }}
+      >
+        <div>
+          <div
+            style={{ padding: "3px" }}
+            onClick={togglePopup}
+            className="text-green-200"
+          >
+            Attack
+          </div>
+          <div style={{ padding: "3px" }} className="text-blue-400">
+            Items
+          </div>
+        </div>
+        <div>
+          <div style={{ padding: "3px" }}>PaLs</div>
+          <div style={{ padding: "3px" }}>Options</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MenuButtonGroup;
