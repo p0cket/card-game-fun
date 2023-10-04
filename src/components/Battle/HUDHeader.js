bb0e83cc71bc6d4d9cebb6e66748d985f7be30a1
pocket <p0ck31@gmail.com>
2023-10-04 00:35:02 -0400

    feat: added more images and choice screen details

--- src/components/Battle/HUDHeader.js
+++ src/components/Battle/HUDHeader.js
@@ -1,26 +1,19 @@
-import React from 'react'
-import {  goldEmoji } from "../../consts/consts";
+import React from "react";
+import { goldEmoji } from "../../consts/consts";
 
-
-function HUDHeader({gameData}) {
+function HUDHeader({ gameData }) {
   return (
     <p
-    style={{
-      margin: "4px",
-      backgroundColor: "#5a7d2a",
-      color: "white",
-      flex: "1",
-    }}
-  >
-    {/* {`${goldEmoji}${gameData.gold}`}
-    <span style={{ color: "#e6ecc3" }}>Gold</span>
-    <span style={{ color: "#e6ecc3" }}>Level</span>
-    {gameData.curScene.lvl}
-    {" -"}
-    <span style={{ color: "#e6ecc3" }}>ACT</span>
-    {gameData.curScene.act} */}
-     Simulation Room: No Effects
-  </p>  )
+      style={{
+        margin: "4px",
+        backgroundColor: "#5a7d2a",
+        color: "white",
+        flex: "1",
+      }}
+    >
+      Simulation Room: No Effects
+    </p>
+  );
 }
 
-export default HUDHeader
+export default HUDHeader;
--- src/components/reward/ChibipalsSelection.js
+++ src/components/reward/ChibipalsSelection.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { testPals } from "../../consts/pals/pals";
 
 const ChibipalsSelection = () => {
   const [selectedMonster, setSelectedMonster] = useState(null);
   const [showDetails, setShowDetails] = useState(false);
 
-  const monsters = [
-    { id: 1, name: 'Monster1', image: 'monster1.png', description: 'Description of Monster1' },
-    { id: 2, name: 'Monster2', image: '', description: 'Description of Monster2' }, // No image available
-    { id: 3, name: 'Monster3', image: 'monster3.png', description: 'Description of Monster3' },
+  // const monsters = [
+  //   { id: 1, name: 'Monster1', image: 'monster1.png', description: 'Description of Monster1' },
+  //   { id: 2, name: 'Monster2', image: '', description: 'Description of Monster2' }, // No image available
+  //   { id: 3, name: 'Monster3', image: 'monster3.png', description: 'Description of Monster3' },
+  // ];
+  const [Luminowl, Glowbuggle, Umbrabunny] = [
+    testPals[0],
+    testPals[1],
+    testPals[2],
   ];
+  const monsters = [Luminowl, Glowbuggle, Umbrabunny];
 
   const handleMonsterSelect = (monster) => {
     setSelectedMonster(monster);
@@ -25,65 +32,176 @@ const ChibipalsSelection = () => {
   };
 
   const popupStyle = {
-    position: 'fixed',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    background: 'green',
-    padding: '20px',
-    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)',
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    background: "green",
+    padding: "5px",
+    boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.5)",
     zIndex: 1000,
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
   };
 
   const monsterListStyle = {
-    display: 'flex',
-    justifyContent: 'center',
+    display: "flex",
+    justifyContent: "center",
   };
 
   const placeholderImageStyle = {
-    backgroundColor: 'green',
-    borderRadius: '50%',
-    width: '100%',
-    paddingTop: '100%',
+    backgroundColor: "green",
+    borderRadius: "50%",
+    width: "100%",
+    paddingTop: "100%",
   };
 
   return (
     <div>
-      <h1 style={{ color: 'white' }}>Choose Your Chibipal</h1>
+      <h1 style={{ color: "white" }}>Choose Your Chibipal</h1>
       <div className="monster-list" style={monsterListStyle}>
         {monsters.map((monster) => (
           <div
             key={monster.id}
-            className={`monster ${selectedMonster === monster ? 'selected' : ''}`}
+            className={`monster ${
+              selectedMonster === monster ? "selected" : ""
+            }`}
             onClick={() => handleMonsterSelect(monster)}
-            style={{ flex: '1', margin: '10px', cursor: 'pointer' }}
+            style={{ flex: "1", margin: "10px", cursor: "pointer" }}
           >
             <div
               style={placeholderImageStyle}
-              className={monster.image ? 'with-image' : 'without-image'}
+              className={monster.image ? "with-image" : "without-image"}
             >
               {monster.image && (
                 <img
                   src={monster.image}
                   alt={monster.name}
-                  style={{ width: '100%', height: '100%', objectFit: 'cover', borderRadius: '50%' }}
+                  style={{ width: "100%", height: "100%", objectFit: "cover" }}
                 />
               )}
             </div>
-            <p style={{ color: 'white' }}>{monster.name}</p>
+            <p style={{ color: "white" }}>{monster.name}</p>
           </div>
         ))}
       </div>
 
       {showDetails && (
         <div style={popupStyle}>
-          <div className="popup-content">
-            <h2>{selectedMonster.name}</h2>
-            <div style={{ width: '100%', height: '100px', backgroundColor: 'green', borderRadius: '50%', marginBottom: '10px' }}></div>
-            <p>{selectedMonster.description}</p>
-            <button onClick={handleSelect} style={{ marginRight: '10px' }}>Select</button>
+          <div
+            className="popup-content"
+            style={{ fontSize: "14px", margin: "10px" }}
+          >
+            <p style={{ fontSize: "16px", marginBottom: "4px" }}>
+              {selectedMonster.name}
+              {/* <span style={{ marginRight: '10px' }}>Cost: {selectedMonster.cost}</span> */}
+              <span style={{ marginRight: "10px" }}>
+                {" "}
+                Lvl: {selectedMonster.level}
+              </span>
+              <span style={{ marginRight: "10px" }}>
+                Exp: {selectedMonster.experience}
+              </span>
+            </p>
+            <div style={{ display: "flex" }}>
+              <img
+                src={selectedMonster.image}
+                alt={selectedMonster.name}
+                style={{
+                  width: "240px",
+                  height: "240px",
+                  objectFit: "cover",
+                  paddingLeft: "5px",
+                  paddingRight: "5px",
+                }}
+              />{" "}
+              <div>
+                <table
+                  style={{ borderCollapse: "collapse", marginRight: "10px" }}
+                >
+                  <tbody>
+                    <tr>
+                      <td>HP:</td>
+                      <td>{selectedMonster.stats.hp}</td>
+                    </tr>
+                    <tr>
+                      <td>Atk:</td>
+                      <td>{selectedMonster.stats.attack}</td>
+                    </tr>
+                    <tr>
+                      <td>Def:</td>
+                      <td>{selectedMonster.stats.defense}</td>
+                    </tr>
+                    <tr>
+                      <td>Sp Atk:</td>
+                      <td>{selectedMonster.stats.special_attack}</td>
+                    </tr>
+                    <tr>
+                      <td>Sp Def:</td>
+                      <td>{selectedMonster.stats.special_defense}</td>
+                    </tr>
+                    <tr>
+                      <td>Speed:</td>
+                      <td>{selectedMonster.stats.speed}</td>
+                    </tr>
+                    {/* <tr>
+                      <td>Size:</td>
+                      <td>{selectedMonster.size}</td>
+                    </tr>
+                    <tr>
+                      <td>Weight:</td>
+                      <td>{selectedMonster.weight}</td>
+                    </tr> */}
+                  </tbody>
+                </table>
+                {selectedMonster.elemental_type}
+                {"-"}
+                {selectedMonster.creature_type} [
+                {selectedMonster.specialty_group}]
+              </div>{" "}
+            </div>
+            <div>
+              <p style={{ padding: "10px" }}>
+                <span>{selectedMonster.description}</span>
+              </p>
+              {/* <span style={{ marginRight: "10px" }}>
+                Quirks: {selectedMonster.quirks.join(", ")}
+              </span> */}
+            </div>
+            <div>{/* <span>Stats:</span> */}</div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                textAlign: "left",
+                gap: "4px",
+              }}
+            >
+              {/* <span >
+                {`Moves: ${selectedMonster.moves.join(" | ")}`}
+              </span> */}
+              <div
+                style={{
+                  backgroundColor: "lightgreen",
+                  padding: "4px",
+                  borderRadius: "4px",
+                  marginBottom: "4px",
+                }}
+              >
+                Moves:{" "}
+                {selectedMonster.moves.map((move, index) => (
+                  <span key={index} style={{ marginRight: "4px" }}>
+                    {move}
+                  </span>
+                ))}
+              </div>
+              <span style={{ color: "lightGray" }}>
+                {` ${selectedMonster.possible_moves.join(", ")}`}
+              </span>
+              <span>Passive: {selectedMonster.passive_ability}</span>
+            </div>
+            {/* <p style={{ margin: "4px 0" }}>Lore: {selectedMonster.lore}</p> */}
+            <button onClick={handleSelect}>Select</button>
             <button onClick={handleGoBack}>Go Back</button>
           </div>
         </div>
