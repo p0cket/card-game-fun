f84bc5b4bd03fc46844c6187a7163fb6888ab884
pocket <p0ck31@gmail.com>
2023-11-01 18:31:02 -0400

    chore: testing out different deploy and Tailwind stuff

---
+++ build/precache-manifest.7b1f3f3b3beb4360ba47c9222f6e7e71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20fe12a95975286171a9",
+    "url": "/card-game-fun/static/css/main.603871af.chunk.css"
+  },
+  {
+    "revision": "20fe12a95975286171a9",
+    "url": "/card-game-fun/static/js/main.974bb326.chunk.js"
+  },
+  {
+    "revision": "627f6e969bcd642ca279",
+    "url": "/card-game-fun/static/js/runtime~main.70af8a22.js"
+  },
+  {
+    "revision": "47f393ebe0eb5a5f1adf",
+    "url": "/card-game-fun/static/js/2.0442777b.chunk.js"
+  },
+  {
+    "revision": "dbe7e0475dcba2df1cc210088f6238f5",
+    "url": "/card-game-fun/index.html"
+  }
+];
