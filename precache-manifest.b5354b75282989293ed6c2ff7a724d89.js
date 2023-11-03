38838b460020cfb580d977384640d1e67b2895d5
pocket <p0ck31@gmail.com>
2023-11-03 08:50:20 -0400

    Updates

--- precache-manifest.b5354b75282989293ed6c2ff7a724d89.js
+++ precache-manifest.b5354b75282989293ed6c2ff7a724d89.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "034bbcc3c674d73183b0",
+    "revision": "858df7dbe0202deca61a",
     "url": "/card-game-fun/static/css/main.603871af.chunk.css"
   },
   {
-    "revision": "034bbcc3c674d73183b0",
-    "url": "/card-game-fun/static/js/main.51d9101a.chunk.js"
+    "revision": "858df7dbe0202deca61a",
+    "url": "/card-game-fun/static/js/main.26983e79.chunk.js"
   },
   {
     "revision": "627f6e969bcd642ca279",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/card-game-fun/static/js/2.01f0e2db.chunk.js"
   },
   {
-    "revision": "47c99a634818100c18a7a3f32b4bde96",
+    "revision": "c93e150e0437d89583cb7039159c0acd",
     "url": "/card-game-fun/index.html"
   }
 ];
