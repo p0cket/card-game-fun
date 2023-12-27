85081ccda3f3b80dfeac89a41c8d4f1abca17c5b
pocket <p0ck31@gmail.com>
2023-12-27 09:19:54 -0500

    fix: prev used enemy health and applied to human pal

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -3,7 +3,7 @@ import { EFFECTS } from '../effects'
 import { basicCards, stunCards } from './allCards'
 const { DRAW, STUN, DOUBLEDAMAGE, SLEEP, POISON } = EFFECTS
 
-export const gameVersion = 'Rebirth: P-A v0.37 - Basic Game Flow 70%'
+export const gameVersion = 'Rebirth: P-A v0.39 - Alex & Statuses'
 
 // deprecated: previous iteration consts.
 export const startingDeck = [
