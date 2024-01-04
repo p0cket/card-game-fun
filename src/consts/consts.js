4636ed04fd20ce6ba8662988e5e4d203598496e4
pocket <p0ck31@gmail.com>
2024-01-03 21:34:12 -0500

    chore: more cleaning to title

--- src/consts/consts.js
+++ src/consts/consts.js
@@ -3,7 +3,7 @@ import { EFFECTS } from '../effects'
 import { basicCards, stunCards } from './allCards'
 const { DRAW, STUN, DOUBLEDAMAGE, SLEEP, POISON } = EFFECTS
 
-export const gameVersion = 'Rebirth: v0.40 - Cleaning Proto'
+export const gameVersion = 'Rebirth: v0.41-p - Almost the essentials'
 
 // deprecated: previous iteration consts.
 export const startingDeck = [
